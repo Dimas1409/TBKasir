c28c042a56d540847a3cb8a3c0edfbb70a0836f2
Dimas Arief Wicaksono <dimasarifw4@gmail.com>
2023-12-26 20:11:30 +0700

    Commit Message

---
+++ InterfacePegawai.java
@@ -0,0 +1,4 @@
+package com.example.hellofx.TB;
+
+public class InterfacePegawai {
+}
---
+++ KasirController.java
@@ -0,0 +1,16 @@
+package com.example.hellofx.TB;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class KasirController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
